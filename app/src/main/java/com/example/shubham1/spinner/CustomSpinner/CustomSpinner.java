862c4aa6a98be29b0216617264f4acb094435fc0
shubham Malhotra <55094276+rksmalhotra0002@users.noreply.github.com>
2019-12-14 23:41:14 +0530

    Initial commit

---
+++ app/src/main/java/com/example/shubham1/spinner/CustomSpinner/CustomSpinner.java
@@ -0,0 +1,108 @@
+package com.example.shubham1.spinner.CustomSpinner;
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.shubham1.spinner.R;
+import com.example.shubham1.spinner.model.Model;
+
+import java.util.ArrayList;
+
+public class CustomSpinner extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_custom_spinner);
+        getids();
+
+    }
+
+private void getids()
+{
+    Spinner custom_spinner = findViewById(R.id.custom_spinner);
+    ArrayList<Model>a=new ArrayList<>();
+    a.add(new Model("india",R.drawable.india));
+    a.add(new Model("america",R.drawable.america));
+    a.add(new Model("pakistan",R.drawable.pakistan));
+
+    CustomAdapter adapter=new CustomAdapter(CustomSpinner.this,a);
+
+    custom_spinner.setAdapter(adapter);
+    custom_spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+        @Override
+        public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+            if(position==0)
+            {
+                Toast.makeText(CustomSpinner.this,"india",Toast.LENGTH_LONG).show();
+            }
+            else if(position==1)
+            {
+                Toast.makeText(CustomSpinner.this,"america",Toast.LENGTH_LONG).show();
+            }
+            else if(position==2)
+            {
+                Toast.makeText(CustomSpinner.this,"pakistan",Toast.LENGTH_LONG).show();
+            }
+
+
+        }
+
+        @Override
+        public void onNothingSelected(AdapterView<?> parent) {
+
+        }
+    });
+
+}
+ class CustomAdapter extends BaseAdapter
+ {
+  private Context context;
+ArrayList<Model>arrayList;
+
+
+     private CustomAdapter(Context context,ArrayList<Model>arrayList) {
+         this.context = context;
+         this.arrayList=arrayList;
+
+     }
+
+     @Override
+     public int getCount() {
+         return arrayList.size();
+     }
+
+     @Override
+     public Object getItem(int position) {
+         return null;
+     }
+
+     @Override
+     public long getItemId(int position) {
+         return 0;
+     }
+
+     @Override
+     public View getView(int position, View convertView, ViewGroup parent) {
+         Model model=arrayList.get(position);
+
+         convertView = LayoutInflater.from(context).inflate(R.layout.demo,parent,false);
+         TextView tv_text =  convertView.findViewById(R.id.tv_text);
+         ImageView iv_image =  convertView.findViewById(R.id.iv_image);
+         tv_text.setText(model.getName());
+         iv_image.setImageResource(model.getImage());
+         return convertView;
+     }
+ }
+}
---
+++ app/src/main/java/com/example/shubham1/spinner/Spinner/Spinners.java
@@ -0,0 +1,68 @@
+package com.example.shubham1.spinner.Spinner;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
+import android.widget.Toolbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.shubham1.spinner.CustomSpinner.CustomSpinner;
+import com.example.shubham1.spinner.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Spinners extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.spinner);
+        getids();
+    }
+
+    private void getids() {
+
+
+        Spinner spinner = findViewById(R.id.spinner);
+        List<String> categories = new ArrayList<>();
+        categories.add( "shubham");
+        categories.add("karan");
+        categories.add("arjun");
+        categories.add("arsh");
+        categories.add(0,"Select Category");
+
+           ArrayAdapter<String>adapter= new ArrayAdapter<>(this,android.R.layout.simple_spinner_item, categories);
+          adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(this);
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        if(position==0)
+        {
+           //no value
+        }
+       else if(position==1)
+        {
+           Intent intent=new Intent(Spinners.this, CustomSpinner.class);
+           startActivity(intent);
+        }
+
+        else {
+            String item = parent.getItemAtPosition(position).toString();
+            Toast.makeText(parent.getContext(), "" + item, Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
+}
